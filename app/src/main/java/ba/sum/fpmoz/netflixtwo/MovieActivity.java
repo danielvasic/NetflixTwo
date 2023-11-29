f410c937bf95b9e5b1a8b19c7306929bf97607c3
danielvasic <dvasic1@gmail.com>
2023-11-29 08:28:57 +0100

    Netflix vježbe 29.11.2023.

--- app/src/main/java/ba/sum/fpmoz/netflixtwo/MovieActivity.java
+++ app/src/main/java/ba/sum/fpmoz/netflixtwo/MovieActivity.java
@@ -1,50 +1,14 @@
 package ba.sum.fpmoz.netflixtwo;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-import com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.database.FirebaseDatabase;
-
-import ba.sum.fpmoz.netflixtwo.adapter.MovieAdapter;
-import ba.sum.fpmoz.netflixtwo.models.Movie;
-
 public class MovieActivity extends AppCompatActivity {
-
-    RecyclerView movieRecyclerView;
-    MovieAdapter adapter;
-    FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
-
-        this.movieRecyclerView = findViewById(R.id.movieListView);
-        this.movieRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        FirebaseRecyclerOptions<Movie> options = new FirebaseRecyclerOptions.Builder<Movie>().setQuery(
-                this.mDatabase.getReference("movies"),
-                Movie.class
-        ).build();
-
-        this.adapter = new MovieAdapter(options);
-        this.movieRecyclerView.setAdapter(this.adapter);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        this.adapter.startListening();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        this.adapter.stopListening();
     }
 }
